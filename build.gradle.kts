ccb94a47219385719a36a1fa28ebf6b03b5d3130
IamMaxim <g.maxim.stepanoff@gmail.com>
2021-09-01 18:02:29 +0300

    Added md5 generation and comparison to skip Bison parser generation if grammar file didn't change since last build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 import org.apache.tools.ant.taskdefs.condition.Os
+import java.security.MessageDigest
+
 
 plugins {
     java
@@ -16,6 +18,9 @@ val reportFilePath = "out/Java_16_Grammar.report"
 // Where to put generated parser
 val javaParserFilePath = "src/main/java/parser/JavaParser.java"
 
+// MD5 of the latest generated grammar file is stored here
+val latestGrammarMD5FilePath = "out/latestGrammarMD5"
+
 
 repositories {
     mavenCentral()
@@ -27,6 +32,26 @@ dependencies {
 }
 
 tasks.getByName("build") {
+    createOutDirs()
+
+    // Only run Bison build if grammar file changed
+    if (readMD5FromFile(latestGrammarMD5FilePath) != grammarFileMD5()) {
+        println("Grammar file changed since last build; Rebuilding parser with Bison...")
+        runBison()
+        writeMD5ToFile(grammarFileMD5(), latestGrammarMD5FilePath)
+    } else {
+        println("Grammar file didn't change; Skipping parser build.")
+    }
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+/**
+ * Creates directories for all Bison output files.
+ */
+fun createOutDirs() {
     // Create out directory if it doesn't exist, so report may be placed inside
     val outPath = reportFilePath.substring(0, reportFilePath.lastIndexOf("/"))
     file(outPath).mkdirs()
@@ -34,10 +59,14 @@ tasks.getByName("build") {
     // Create parser directory if it doesn't exist, so parser may be placed inside
     val parserPath = javaParserFilePath.substring(0, javaParserFilePath.lastIndexOf("/"))
     file(parserPath).mkdirs()
+}
 
-    // Run Bison
+/**
+ * Runs Bison using OS-specific shell command.
+ */
+fun runBison() =
     when {
-        Os.isFamily(Os.FAMILY_WINDOWS) -> {
+        Os.isFamily(Os.FAMILY_WINDOWS) ->
             exec {
                 workingDir = File(".")
                 executable = "bin/win_bison.exe"
@@ -49,8 +78,7 @@ tasks.getByName("build") {
                     javaGrammarFilePath
                 )
             }
-        }
-        Os.isFamily(Os.FAMILY_MAC) -> {
+        Os.isFamily(Os.FAMILY_MAC) ->
             exec {
                 workingDir = File(".")
                 executable = "bin/bison_mac"
@@ -62,15 +90,44 @@ tasks.getByName("build") {
                     javaGrammarFilePath
                 )
             }
-        }
         // TODO: add Linux support
-        else -> {
+        else ->
             throw kotlin.UnsupportedOperationException("Your OS is not yet supported. File a GitHub or issue or " +
                     "provide a Pull Request with support for Bison execution for your OS.")
-        }
     }
+
+
+/**
+ * Returns MD5 string for a given file.
+ */
+fun generateMD5(filepath: String): String {
+    val digest: MessageDigest = MessageDigest.getInstance("MD5")
+
+    File(filepath).inputStream().use { inputStream ->
+        val buffer = ByteArray(8192)
+        var read = 0
+        do {
+            digest.update(buffer, 0, read);
+            read = inputStream.read(buffer)
+        } while (read > 0)
+    }
+
+    val md5sum: ByteArray = digest.digest()
+    val bigInt = BigInteger(1, md5sum)
+
+    return bigInt.toString(16).padStart(32, '0')
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
+fun grammarFileMD5(): String =
+    generateMD5(javaGrammarFilePath)
+
+fun readMD5FromFile(filepath: String): String =
+    File(filepath).let { f ->
+        if (f.exists())
+            File(filepath).readText()
+        else
+            ""
+    }
+
+fun writeMD5ToFile(md5: String, filepath: String) =
+    File(filepath).writeText(md5)
