6b23bd8b774e4f661b0f037faf3e93aa74fae7b9
Zouev <e.zuev@innopolis.ru>
2021-09-05 13:26:57 +0300

    Small testing updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,9 +78,8 @@ fun runBison() =
                 workingDir = File(".")
                 executable = "bin/win_bison.exe"
                 args = mutableListOf(
-                    "-r",
-                    "all",
-                    "--debug",
+                    "-r all",
+                 //   "--debug", "--help", "--stacktrace",
                     "--report-file=${reportFilePath}",
                     "--output=${javaParserFilePath}",
                     javaGrammarFilePath
@@ -91,8 +90,7 @@ fun runBison() =
                 workingDir = File(".")
                 executable = "bin/bison_mac"
                 args = mutableListOf(
-                    "-r",
-                    "all",
+                    "-r all",
                     "--report-file=${reportFilePath}",
                     "--output=${javaParserFilePath}",
                     javaGrammarFilePath
@@ -103,8 +101,7 @@ fun runBison() =
                 workingDir = File(".")
                 executable = "bison"
                 args = mutableListOf(
-                    "-r",
-                    "all",
+                    "-r all",
                     "--report-file=${reportFilePath}",
                     "--output=${javaParserFilePath}",
                     javaGrammarFilePath
@@ -126,7 +123,7 @@ fun generateMD5(filepath: String): String {
         val buffer = ByteArray(8192)
         var read = 0
         do {
-            digest.update(buffer, 0, read);
+            digest.update(buffer, 0, read)
             read = inputStream.read(buffer)
         } while (read > 0)
     }
