f526cac1e0505746806883eca586a7ba712f2d7f
someilay <ismilioshyn@gmail.com>
2021-10-12 00:02:50 +0300

    Assignment operators & Arrays tests

---
+++ src/test/resources/ch_15_expressions/sec_15_10_array_creation_and_access_expressions/sec_15_10_3_array_access_expressions/SimpleFloatArrayAccess.java
@@ -0,0 +1,9 @@
+
+public class SimpleFloatArrayAccess {
+	public static void main(String[] args) {
+		float[] array = new float[5];
+		array[0] = 1f;
+		System.out.println(array[0]);
+		System.out.println("passed");
+	}
+}
