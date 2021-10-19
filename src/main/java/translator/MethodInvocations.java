b57ac7debc35b6f181ed4d652248ae27e3dccd01
someilay <ismilioshyn@gmail.com>
2021-10-19 19:41:09 +0300

    Translators for literals & MethodInvocations

---
+++ src/main/java/translator/MethodInvocations.java
@@ -0,0 +1,30 @@
+package translator;
+
+import eotree.*;
+import tree.Expression.Primary.MethodInvocation;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+public class MethodInvocations {
+    public static EOCopy mapMethodInvocation(MethodInvocation methodInvocation){
+        if (methodInvocation.qualifier != null){
+            throw new IllegalArgumentException("Qualifiers aren't supported yet");
+        }
+        if (methodInvocation.superSign){
+            throw new IllegalArgumentException("Super sign isn't supported yet");
+        }
+        if (methodInvocation.typeArguments != null){
+            throw new IllegalArgumentException("Type arguments aren't supported yet");
+        }
+
+        return new EOCopy(
+                new EODot(Optional.empty(), methodInvocation.name),
+                methodInvocation.arguments.arguments.stream()
+                        .map(expression -> new EOAnonExpr(new EODot(Optional.empty(), "not implemented")))
+                        .collect(Collectors.toList())
+        );
+        // expression -> Expressions.mapExpresion(expression)
+        // Optional.empty() in EODot will be removed
+    }
+}
