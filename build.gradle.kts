aba917feb1064b949934b09b0c3fee8a17689ac5
Egor Klementev <egorklementeff@yandex.ru>
2022-01-26 19:18:05 +0300

    TESTS: junit update

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:1.7.33")
     implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
 
+    implementation("org.junit.platform:junit-platform-commons:1.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     implementation(kotlin("stdlib-jdk8"))
