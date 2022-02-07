cee0e1b5e90ac99fd71db3653d5a65fdf860b94b
IamMaxim <g.maxim.stepanoff@gmail.com>
2022-02-07 15:51:16 +0300

    Updated CI configuration

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Fail on non-zero exit codes
+set -e
+
 # Tag should be exported from outside
 
-./gradlew build publish -Dcandidates=false --info -PmvnPublicationVersion=${tag}
+echo "Building and publishing J2EO"
+./gradlew build fatJar publish -Dcandidates=false --info -PmvnPublicationVersion=${tag}
+echo "Publish completed"
