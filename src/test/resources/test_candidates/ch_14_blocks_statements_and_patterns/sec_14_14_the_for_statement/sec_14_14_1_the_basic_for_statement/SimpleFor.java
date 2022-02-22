ad0d50cfbcc28828a6bb969f6bcbd94ceb3affb1
Egor Klementev <egorklementeff@yandex.ru>
2022-02-22 20:27:26 +0300

    TEST: chapter 14 tests

---
+++ src/test/resources/test_candidates/ch_14_blocks_statements_and_patterns/sec_14_14_the_for_statement/sec_14_14_1_the_basic_for_statement/SimpleFor.java
@@ -0,0 +1,8 @@
+public class SimpleFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("[#" + (i + 1) + "]: uWu");
+        }
+        System.out.println("passed");
+    }    
+}
---
+++ src/test/resources/test_candidates/ch_14_blocks_statements_and_patterns/sec_14_9_the_if_statement/sec_14_9_1_the_if_then_statement/SimpleIf.java
@@ -0,0 +1,8 @@
+public class SimpleIf {
+	public static void main(String[] args) {
+		boolean truth = true;
+		if (truth) {
+			System.out.println("passed");
+		}
+	}
+}
