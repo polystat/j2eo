a3fbc2160380707140d9cdece82b93a67577fc02
IamMaxim <g.maxim.stepanoff@gmail.com>
2022-04-19 22:39:38 +0300

    New stdlib, dynamic support for all primitives, fixed imports, fixed generation of arithmetic expressions

--- src/main/java/translator/Literals.kt
+++ src/main/java/translator/Literals.kt
@@ -12,33 +12,39 @@ import util.TokenCodes
 fun mapLiteral(literal: Literal): EOObject =
     when (literal.code) {
         TokenCode.StringLiteral -> generateEOData(
-                listOf(TokenCodes.CLASS__STRING.value, "constructor2").eoDot(),
+                listOf(TokenCodes.CLASS__STRING.value, "constructor_2").eoDot(),
+                listOf(TokenCodes.CLASS__STRING.value, "new").eoDot(),
                 EOStringData(literal.value as String)
         )
         TokenCode.FloatingLiteral -> generateEOData(
-                listOf(TokenCodes.PRIM__FLOAT.value, "constructor2").eoDot(),
+                listOf(TokenCodes.PRIM__FLOAT.value, "constructor_2").eoDot(),
+                listOf(TokenCodes.PRIM__FLOAT.value, "new").eoDot(),
                 EOFloatData((literal.value as String).toFloat())
         )
         TokenCode.IntegerLiteral -> generateEOData(
-                listOf(TokenCodes.PRIM__INT.value, "constructor2").eoDot(),
+                listOf(TokenCodes.PRIM__INT.value, "constructor_2").eoDot(),
+                listOf(TokenCodes.PRIM__INT.value, "new").eoDot(),
                 EOIntData(try {(literal.value as String).toInt()} catch (e: NumberFormatException) { 0 /* FIXME: parse integer literals */ })
         )
         TokenCode.True -> generateEOData(
-                listOf(TokenCodes.PRIM__BOOLEAN.value, "constructor2").eoDot(),
+                listOf(TokenCodes.PRIM__BOOLEAN.value, "constructor_2").eoDot(),
+                listOf(TokenCodes.PRIM__BOOLEAN.value, "new").eoDot(),
                 EOBoolData(true)
         )
         TokenCode.False -> generateEOData(
-                listOf(TokenCodes.PRIM__BOOLEAN.value, "constructor2").eoDot(),
+                listOf(TokenCodes.PRIM__BOOLEAN.value, "constructor_2").eoDot(),
+                listOf(TokenCodes.PRIM__BOOLEAN.value, "new").eoDot(),
                 EOBoolData(false)
         )
         TokenCode.Null -> generateEOData( /* FIXME: use proper null */
-            listOf(TokenCodes.PRIM__INT.value, "constructor2").eoDot(),
+            listOf(TokenCodes.PRIM__INT.value, "constructor_2").eoDot(),
+            listOf(TokenCodes.PRIM__INT.value, "new").eoDot(),
             EOIntData(0)
         )
         else -> throw IllegalArgumentException("Mapping of type ${literal.javaClass.simpleName} (${literal.code}) is not supported.")
     }
 
-fun generateEOData(primitiveTypeCall: EODot, value: EOData) : EOObject =
+fun generateEOData(primitiveTypeCall: EODot, primitiveNewCall: EODot, value: EOData) : EOObject =
     EOObject(
         listOf(),
         None,
@@ -46,6 +52,7 @@ fun generateEOData(primitiveTypeCall: EODot, value: EOData) : EOObject =
             EOBndExpr(
                 EOCopy(
                     primitiveTypeCall,
+                    primitiveNewCall,
                     value
                 ),
                 "@"
--- src/test/resources/test_ready/SimpleTest.java
+++ src/test/resources/test_ready/SimpleTest.java
@@ -1,6 +1,8 @@
 public class SimpleTest {
 	
 	public static void main(String[] args) {
+		int i = 0;
+		int j = i++;
 		System.out.println("passed");
 	}
 }
