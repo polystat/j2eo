6eb530420c7b51b0733bc6854fe2fb76634e962d
someilay <ismilioshyn@gmail.com>
2022-06-16 17:35:05 +0300

    New expressions name decomposition (Hot fix 1)

--- src/main/java/translator/Context.kt
+++ src/main/java/translator/Context.kt
@@ -5,7 +5,9 @@ import tree.Entity
 class Context(var uniqueObjectId: Int) {
     fun genUniqueEntityName(e: Entity?): String {
         return if (e != null) {
-            "atom_${e.javaClass.simpleName}_${uniqueObjectId++}"
+            var simpleName = e.javaClass.simpleName
+            simpleName = simpleName[0].lowercase() + simpleName.substring(1)
+            "${simpleName}_${uniqueObjectId++}"
         } else {
             "no_name_for_null_entity"
         }
